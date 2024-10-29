1fed106b2ee2ec5dca7d0d8680ee55e1deebc308
dvaklon82@gmail.com <a.aisultan@ad.interteach.kz>
2024-10-29 10:49:25 +0500

    Contact page modified

--- lib/pages/contacts.dart
+++ lib/pages/contacts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
+import 'package:flutter_glow/flutter_glow.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ContactsPage extends StatelessWidget {
@@ -7,6 +8,20 @@ class ContactsPage extends StatelessWidget {
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
+  }
+
+  Future<void> _launchUrl(String hrefTo) async {
+    final Uri url = Uri.parse(hrefTo);
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +31,7 @@ class ContactsPage extends StatelessWidget {
         automaticallyImplyLeading: false,
         centerTitle: true,
         title: Text(
-          'Addresses',
+          'Сontact us',
           style: TextStyle(color: Colors.white),
         ),
         leading: IconButton(
@@ -34,7 +49,91 @@ class ContactsPage extends StatelessWidget {
           },
         ),
       ),
-      body: Text('data'),
+      backgroundColor: Colors.black,
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              GlowText('Get in Touch',
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 36)),
+              RichText(
+                  text: TextSpan(children: [
+                TextSpan(
+                    text:
+                        'If you have any questions or suggestions in building app,\nfeel free to ask')
+              ]))
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 3),
+                  borderRadius: BorderRadius.circular(30)),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 15.0),
+                  ),
+                  onPressed: () {
+                    _makePhoneCall('+77774094835');
+                  },
+                  child: Row(children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+7 (777) 409 48 35',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ])),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 3),
+                  borderRadius: BorderRadius.circular(30)),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 15.0),
+                  ),
+                  onPressed: () {
+                    _launchUrl('https://wa.me/77774094835');
+                  },
+                  child: Row(children: [
+                    Icon(
+                      Icons.recent_actors,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Whatsapp',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ])),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
